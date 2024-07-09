e35946701edd246dd7458b9fd93b42b44ca0b548
MarinaPorokhnya <marina.vbyaby.poroxnya@mail.ru>
2024-07-09 14:20:16 +0300

    Добавили решение задачи3c7

---
+++ Задача3с7/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Задайте произвольный массив.
+//  Выведете его элементы, начиная с конца. 
+//  Использовать рекурсию, не использовать циклы.
+
+void Reverse (int[] array, int i)
+{
+    if(i < 0) return;
+    Console.Write(array[i] + " ");
+    Reverse(array, i - 1);
+}
+
+int[] array = {34, 5, 67, 32, 0};
+Reverse(array, array.Length - 1);
